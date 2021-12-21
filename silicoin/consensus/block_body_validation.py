bbf2a94b87b55e2371ee2ba4a9c8556bf6f78dfc
zcomputerwiz <zcomputerwiz@users.noreply.github.com>
2021-12-21 09:43:24 -0600

    fix rogue rename

--- silicoin/consensus/block_body_validation.py
+++ silicoin/consensus/block_body_validation.py
@@ -3,7 +3,7 @@ import logging
 from typing import Dict, List, Optional, Set, Tuple, Union, Callable
 
 from blspy import G1Element
-from silicoinbip158 import PyBIP158
+from chiabip158 import PyBIP158
 from clvm.casts import int_from_bytes
 
 from silicoin.consensus.block_record import BlockRecord
